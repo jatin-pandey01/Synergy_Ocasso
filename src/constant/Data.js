bb1fe2b6d479f46eaf4d227654d20220408653a4
jatin-pandey01 <=>
2024-02-25 01:17:39 +0530

    Dashboard changes

---
+++ src/constant/Data.js
@@ -0,0 +1,50 @@
+export const details = [
+    {
+      Id:1,
+      Name:'Synergy',
+      Manager : 'Dhruv Chauhan',
+      DateOfCreation :'24th Feb 2024',
+    },
+    {
+      Id:2,
+      Name:'DJSCE',
+      Manager : 'Jatin Pandey',
+      DateOfCreation : '25th Feb 2024'
+    },
+    {
+      Id : 3,
+      Name :'ISACA',
+      Manager:'Bhavin Job',
+      DateOfCreation:'26th Feb 2024',
+    },
+    {
+      Id : 4,
+      Name : 'DJS NSDC',
+      Manager : 'Sacchit Wathe',
+      DateOfCreation : '27th Feb 2024'
+    },
+    {
+      Id : 5,
+      Name : 'SigAI',
+      Manager : 'Ansh Shah',
+      DateOfCreation : '28th Feb 2024'
+    },
+    {
+      Id : 6,
+      Name : 'Codestars',
+      Manager : 'Viram Mehta',
+      DateOfCreation : '29th Feb 2024'
+    },
+    {
+      Id : 7,
+      Name : 'DJS GDSC',
+      Manager : 'Suraj Shinde',
+      DateOfCreation : '30th Feb 2024'
+    },
+    {
+      Id : 8,
+      Name : 'DJS ACM',
+      Manager : 'Chirayu Agrawal',
+      DateOfCreation : '1st March 2024'
+    }
+]
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -3,7 +3,10 @@ import { Link } from 'react-router-dom';
 import { NewsContext } from '../context/Context';
 import Login from '../component/Login';
 import Register from '../component/Register';
-import logo from '../assets/ocassoLogo.jpg'
+import logo from '../assets/ocassoLogo.jpg';
+import { IoMdAdd } from "react-icons/io";
+import {details} from './../constant/Data';
+import { FaArrowRight } from "react-icons/fa6";
 
 export const Navbar = ()=>{
     const { auth , setAuth ,
@@ -12,8 +15,8 @@ export const Navbar = ()=>{
     console.log(user);
     // console.log(auth);
     return (
-        <div className='bg-[#010440] text-white flex justify-around py-3'>
-        <img src={logo} width={100} height={100} />
+        <div className='bg-[#010440] text-white flex justify-around py-3 items-center'>
+        <Link to={'/'}> <img src={logo} width={200} height={100} className='rounded-xl' /> </Link>
         <div> 
           {
             !auth ?
@@ -41,68 +44,66 @@ export const Navbar = ()=>{
 }
 
 const Home = () => {
-  const [showForm,setShowForm] = useState(false); 
-    const details = [
-        {
-          id:1,
-          name:'Synergy',
-          totalInteraction:'245k',
-        },
-        {
-          id:2,
-          name:'DJSCE',
-          totalInteraction : '500k'
-        },
-        {
-          id:1,
-          name:'Synergy',
-          totalInteraction:'245k',
-        },
-        {
-          id:2,
-          name:'DJSCE',
-          totalInteraction : '500k'
-        },
-        {
-          id:1,
-          name:'Synergy',
-          totalInteraction:'245k',
-        },
-        {
-          id:2,
-          name:'DJSCE',
-          totalInteraction : '500k'
-        },
-        {
-          id:1,
-          name:'Synergy',
-          totalInteraction:'245k',
-        },
-        {
-          id:2,
-          name:'DJSCE',
-          totalInteraction : '500k'
-        }
-    ]
+    const [showForm,setShowForm] = useState(false);
+    const [showQR,setShowQR] = useState(false);
     return (
     <div>
       <Navbar/>
-      <div className='flex justify-around px-10 pt-10 bg-[#F2F0F4]'>
-        <div>
-          <p className='cursor-pointer'> Create New Contract </p>
+      <div className='flex flex-col justify-around px-10 pt-10 h-full bg-[#F2F0F4]'>
+        <div className='flex flex-col justify-center items-center'>
+          <div className='flex gap-5 items-center'>
+            <div className={`bg-blue-500 flex justify-center items-center px-2 py-1 rounded-lg ${showForm || showQR ? 'mb-10' : ''}`} >
+              <p className='cursor-pointer' onClick={()=>{setShowForm(!showForm); setShowQR(false);}}> Create New Contract </p>
+              <IoMdAdd />
+            </div>
+            <div className={`bg-blue-500 flex justify-center items-center px-2 py-1 rounded-lg ${showForm || showQR ? 'mb-10' : ''}`} >
+              <p className='cursor-pointer' onClick={()=>{setShowForm(false); setShowQR(!showQR);}}> </p>
+              <a href='https://hovercode.com/ai-qr-code-generator/' target='_blank'> Create AI QR  </a>
+              <IoMdAdd />
+            </div>
+          </div>
+          <div>
+          {
+            showForm ? <div className='bg-gray-700 px-5 py-2 rounded-xl flex flex-col items-center gap-2'>
+              <input type='text' placeholder='Contract Name' className='px-1 outline-none border rounded-lg py-1' />
+              <input type='text' placeholder='Contract Amount' className='px-1 outline-none border rounded-lg py-1' />
+              <input type='text' placeholder='Contract Duration' className='px-1 outline-none border rounded-lg py-1' />
+              <button className='bg-blue-700 text-white px-2 py-1 rounded-xl hover:bg-opacity-50'> Submit </button>
+            </div> : <></>
+          }
+          </div>
+          <div>
           
+          </div>
         </div>
-        <div className='flex flex-col'>
-          <p className='font-medium'> Companies Details  </p>
+        <div className=''>
+          <p className='font-medium'> Current Compaign </p>
           <div className=''>
-            {
-              details.map((d)=>{
-                return <div className='flex justify-between' key={d.id} >
-                  <Link to={'/synergy'} className='cursor-pointer'> {d.name} </Link>
-                  <Link to={'/synergy'} className='cursor-pointer'> {d.totalInteraction} </Link>
-                </div>
-              })
-            }
+            <table border="1" cellPadding={'2px'} cellSpacing={'5px'} className=' w-full'>
+              <thead>
+                <tr className='border-b-2 border-t-2 border-l-2 text-2xl border-r-2 py-2 border-black'>
+                  <th className='py-5'>Sr. No.</th>
+                  <th> Name </th>
+                  <th> Manager </th>
+                  <th> Date of Creation </th>
+                  <th> Link </th>
+                </tr>
+              </thead>
+              <tbody>
+                {
+                  details.map((d)=>(
+                    <tr key={d.Id} className='gap-5 border-b-2 text-xl text-center border-r-2 border-l-2 border-black'>
+                      <td className=' py-4'> {d.Id} </td>
+                      <td> {d.Name} </td>
+                      <td> {d.Manager} </td>
+                      <td align="position"valign="position"> {d.DateOfCreation} </td>
+                      <Link to={'/synergy'} className='text-center'> <FaArrowRight /> </Link>
+                      {/* <a href='QR Code Generator _ Create Free Dynamic QR Codes.html'> <FaArrowRight/> </a> */}
+                    </tr>
+                  ))
+                }
+              </tbody>
+            </table>
           </div>
         </div>
       </div>
